a3ea0b473dabf26afefcaa083b2b20c01d584b02
Petr Reichl <petr@tapmates.com>
2012-01-16 18:11:47 +0100

    base project structure
    added project

---
+++ Source/libOrdrin/Ordrin/Ordrin.h
@@ -0,0 +1,13 @@
+//
+//  Ordrin.h
+//  Ordrin
+//
+//  Created by Reichl Petr on 16.01.12.
+//  Copyright (c) 2012 tapmates. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Ordrin : NSObject
+
+@end
---
+++ Source/libOrdrin/Ordrin/Ordrin.m
@@ -0,0 +1,23 @@
+//
+//  Ordrin.m
+//  Ordrin
+//
+//  Created by Reichl Petr on 16.01.12.
+//  Copyright (c) 2012 tapmates. All rights reserved.
+//
+
+#import "Ordrin.h"
+
+@implementation Ordrin
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
